6f9f3ee38b9925c698f3cbe2c7ff4e0b6c110fe4
Mohammad Azam <azamsharp@gmail.com>
2021-12-21 20:26:35 -0600

    Initial Commit

---
+++ SavingImages/SavingImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavingImagesApp.swift
+//  SavingImages
+//
+//  Created by Mohammad Azam on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct SavingImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
